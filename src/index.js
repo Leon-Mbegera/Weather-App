2da1fb71ecd720492717f75ca2ef142366c01dc8
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-09-06 14:32:53 +0300

    Console log a users input location

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@
 const form = document.getElementById('input-form');
 form.addEventListener('click', (e) => {
   e.preventDefault();
-  const location = document.getElementById('location');
+  const location = document.getElementById('location').value;
+  console.log(location);
   createApi(location);
 });
+
+const createApi = (location) => {
+  const defaultUrl = "api.openweathermap.org/data/2.5/weather?q=";
+  const realUrl = defaultUrl + location + "&APPID=3ac2771651cac45621767706203925fe";
+  // console.log(realUrl);
+};
