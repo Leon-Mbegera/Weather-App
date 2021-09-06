cae98bab857402fe10ca26d8cd1c24fce13c778f
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-09-06 17:02:38 +0300

    Fetch and show object with weather conditions on browser console

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-
 const form = document.getElementById('input-form');
-form.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = document.getElementById('location').value;
-  console.log(location);
   createApi(location);
 });
 
 const createApi = (location) => {
-  const defaultUrl = "api.openweathermap.org/data/2.5/weather?q=";
+  const defaultUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
   const realUrl = defaultUrl + location + "&APPID=3ac2771651cac45621767706203925fe";
-  // console.log(realUrl);
+  getWeather(realUrl);
 };
+
+const getWeather = async (realUrl) => {
+
+  const response = await fetch(realUrl);
+  const conditions = await response.json();
+  console.log(conditions);
+};
+
+
