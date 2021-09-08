d40b8a99f028cf20d2c4546ff078397c8395bed0
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-09-08 20:31:59 +0300

    Remove getWeather method

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ const weatherData = document.getElementById('weather-data');
 let convert = false;
 let temp = 0;
 
-const createApi = (location) => {
+const createApi = async (location) => {
   const defaultUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
   const realUrl = `${defaultUrl + location}&APPID=3ac2771651cac45621767706203925fe`;
-  getWeather(realUrl)
-    .then((conditions) => {
-      (conditions);
-      getTempAndOthers(conditions);
-    }).catch((err) => {
-      console.error(err);
-    });
+  const response = await fetch(realUrl);
+  const conditions = response.json();
+  conditions.then((conditions) => {
+    console.log(conditions);
+    getTempAndOthers(conditions);
+  }).catch((err) => {
+    console.error(err);
+  });
 };
 
 form.addEventListener('submit', (e) => {
@@ -20,12 +21,6 @@ form.addEventListener('submit', (e) => {
   const location = document.getElementById('location').value;
   createApi(location);
 });
-
-const getWeather = async (realUrl) => {
-  const response = await fetch(realUrl);
-  const conditions = await response.json();
-  return conditions;
-};
 
 const getTempAndOthers = (conditions) => {
   temp = Math.floor((conditions.main.temp) - 273);
